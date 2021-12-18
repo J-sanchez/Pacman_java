df06580a50129beefc0b715fe4af6da6746fbd78
J-sanchez <jonathan.sanchez@windstream.net>
2021-12-17 21:14:36 -0700

    Setup Model and Pacman

---
+++ Model.java
@@ -0,0 +1,11 @@
+package pacman_java;
+
+import java.awt.event.ActionListener;
+import java.swing.Jpanel;
+import java.awt.Dimensions;
+import java.awt.ActionEvent;
+
+
+public class Model extends JPanel implements ActionListener {
+    
+}
